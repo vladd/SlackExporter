d237aa1c395108a1dc2eb23fd4504f9c3eff7aa1
VladD <vladd-github@gmx.de>
2018-09-20 20:42:59 +0200

    Added user-agent to downloader in order to calm down some suspicious sites

--- SlackExporter/RemoteCache.cs
+++ SlackExporter/RemoteCache.cs
@@ -30,7 +30,15 @@ namespace SlackExporter
             }
         }
 
-        HttpClient httpClient = new HttpClient(new HttpClientHandler() { AllowAutoRedirect = true });
+        HttpClient httpClient =
+            new HttpClient(new HttpClientHandler() { AllowAutoRedirect = true })
+            {
+                DefaultRequestHeaders =
+                {
+                    { "User-Agent",
+                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" }
+                }
+            };
 
         public string Cache(string remoteUri, string suggestedName)
         {
